cdd4e2560756d2b60090c801c1e1fc815c11a02a
subhamc88 <subham020305@gmail.com>
2024-03-06 23:13:41 +0530

    index html

--- ladybug.js
+++ ladybug.js
@@ -3,25 +3,20 @@ document.addEventListener("DOMContentLoaded", function() {
   const bug = document.getElementById("bug");
   const codeEditor = document.getElementById("code-editor");
 
-  // Function to move the bug randomly within the code editor
   function moveBug() {
-    const editorRect = codeEditor.getBoundingClientRect();
-    const maxX = editorRect.width - bug.offsetWidth;
-    const maxY = editorRect.height - bug.offsetHeight;
+    const maxX = codeEditor.offsetWidth;
+    const maxY = codeEditor.offsetHeight;
     const randomX = Math.floor(Math.random() * maxX);
     const randomY = Math.floor(Math.random() * maxY);
 
-    // Use Anime.js for smooth animation
     anime({
       targets: bug,
       translateX: randomX,
       translateY: randomY,
-      duration: 2000, // Duration of the animation in milliseconds
-      easing: 'linear' // Linear easing for smoother movement
+      duration: 2000,
+      easing: 'linear'
     });
   }
-
-  // Trigger bug movement on user interaction or at regular intervals
-  setInterval(moveBug, 3000); // Move bug every 3 seconds
+  setInterval(moveBug, 3000);
 });
 
